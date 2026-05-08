34c7acb86e41f5e8f2166171f46e3fb131da4be0
vahamut <vahamut@disroot.org>
2026-05-08 14:46:11 +0900

    update module `strage.h/c`

--- server/include/engine/storage.h
+++ server/include/engine/storage.h
@@ -2,6 +2,15 @@
 #define SQ_STORAGE_H
 
 #include "protocol.h"
+#include <sqlite3.h>
+
+typedef struct server_context_t sq_server_context_t;
+
+typedef struct sq_db_context {
+	sqlite3 *h_db;
+	bool is_open;
+} sq_db_context_t;
+
 
 typedef struct {
 	int id;
@@ -12,8 +21,9 @@ typedef struct {
 	char *output;
 } sq_table_commands;
 
-int sq_db_init(const char *db_path);
-void sq_db_close(void);
-int sq_db_save_backlog(const sq_context_t *ctx, const char *cmd, const char *output);
+bool sq_db_init(sq_server_context_t *ctx_server, char *db_path);
+void sq_db_close(sq_server_context_t *ctx_server);
+int sq_db_save_backlog(sq_server_context_t *ctx_server, const sq_overview_context_t *ctx, const char *cmd, const char *output);
+void sq_load_backlog(sq_server_context_t *ctx_server);
 
 #endif
