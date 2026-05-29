be6a52005dfe692c922f5f7a536a9bc0297eca26
2racss <vahamut@disroot.org>
2026-05-29 15:11:18 +0900

    update `sq_u16_t require_packect_header()`

--- include/validate.h
+++ include/validate.h
@@ -8,16 +8,6 @@
 #include <stddef.h>
 
 
-sq_u16_t require_packect_header(sq_packet_header_t *h) {
-	if (!h) {
-		return (SQ_INVALID_PARAM | SQ_RETURN_CAT_RESPONSE);
-	} else if (h->magic != SQ_MAGIC) {
-		return (SQ_INVALID_MAGIC | SQ_RETURN_CAT_RESPONSE);
-	} else if (h->payload_size < 1) {
-		return(SQ_INVALID_SIZE | SQ_RETURN_CAT_RESPONSE);
-	} else {
-		return (SQ_SUCCESS | SQ_RETURN_CAT_RESPONSE);
-	}
-}
+sq_u16_t require_packect_header(sq_packet_header_t *h);
 
 #endif
--- src/validate.c
+++ src/validate.c
@@ -1,1 +1,17 @@
+#include "common.h"
+#include "protocol.h"
+#include "defines.h"
+#include "validate.h"
+
+sq_u16_t require_packect_header(sq_packet_header_t *h) {
+	if (!h) {
+		return (SQ_INVALID_PARAM | SQ_RETURN_CAT_RESPONSE);
+	} else if (h->magic != SQ_MAGIC) {
+		return (SQ_INVALID_MAGIC | SQ_RETURN_CAT_RESPONSE);
+	} else if (h->payload_size < 1) {
+		return(SQ_INVALID_SIZE | SQ_RETURN_CAT_RESPONSE);
+	} else {
+		return (SQ_SUCCESS | SQ_RETURN_CAT_RESPONSE);
+	}
+}
 
