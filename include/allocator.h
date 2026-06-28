bd9c16daa8b5268aeec1d5ac30f438df15ee64ef
2racss <vahamut@disroot.org>
2026-06-29 08:30:29 +0900

    i/allocator: thread-safed

--- include/allocator.h
+++ include/allocator.h
@@ -1,6 +1,7 @@
 #ifndef CORE_ALLOCATOR_H
 #define CORE_ALLOCATOR_H
 
+#include <pthread.h>
 #include <stddef.h>
 #include "status.h"
 
@@ -8,6 +9,9 @@
 static inline size_t align(size_t size) {
 	return (size + 15) & ~15;
 }
+
+
+typedef int arena_id_t; 
 
 /**
  * Sizes
@@ -23,6 +27,8 @@ constexpr bool ARENA_REQUEST_RESET_OFFSET = true;
 constexpr bool ARENA_REQUEST_RESET_OFFSET_NO = false;
 constexpr bool ARENA_FORCE_DESTROY = true;
 constexpr bool ARENA_FORCE_DESTROY_NO = false;
+constexpr arena_id_t ARENA_ID_CURRENT = -1;
+constexpr arena_id_t ARENA_ID_ENTIRE  = -2;
 
 
 static void *default_malloc(void *ptr, size_t size, void *ctx);
@@ -53,6 +59,7 @@ typedef struct arena_block {
 } arena_block_t;
 
 typedef struct {
+	pthread_mutex_t mutex;
 	arena_block_t *head;
 	arena_block_t *current;
 	size_t block_size;
@@ -73,12 +80,14 @@ typedef struct {
 arena_t *arena_init(size_t block_size);
 void *arena_alloc_impl(arena_t *arena, size_t size, const char *file, int line, const char *func);
 void *arena_alloc(arena_t *arena, size_t size);
-status_t arena_set_contains_fd(arena_t *arena, bool require_set);
-status_t arena_set_contains_db_connection(arena_t *arena, bool require_set);
-status_t arena_set_contains_resources(arena_t *arena, bool require_set);
+status_t arena_set_contains_fd(arena_t *arena, bool is_contain);
+status_t arena_set_contains_db_connection(arena_t *arena, bool is_contain);
+status_t arena_set_contains_resources(arena_t *arena, bool is_contain);
+status_t arena_is_contains_resources(arena_t *arena);
 size_t get_amount_capacity(arena_block_t *head);
-status_t arena_shred(arena_t *arena, unsigned short id, bool request_reset_offset);
-status_t block_shred(arena_block_t *block, bool request_reset_offset);
+void arena_reset(arena_t *arena);
+status_t arena_shred(arena_t *arena, arena_id_t id, bool request_reset_offset);
 status_t arena_destroy(arena_t *arena, bool force_destory);
+char *arena_strdup(arena_t *arena, const char *src);
 
 #endif
--- src/allocator.c
+++ src/allocator.c
@@ -1,5 +1,6 @@
 #include "allocator.h"
 #include "status.h"
+#include <pthread.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
@@ -23,23 +24,48 @@ static arena_block_t *get_arena(arena_block_t *block_head, unsigned short id) {
 }
 
 static arena_block_t *arena_new_block(size_t size) {
-	size_t total_size = sizeof(arena_block_t) + size;
-	size_t aligned = align(total_size);
-	arena_block_t *block = malloc(aligned);
-	if (!block) return nullptr;
+    size_t header_size = align(sizeof(arena_block_t));
+    size_t data_size = align(size);
+    size_t total_size = header_size + data_size;
 
-	volatile unsigned char *ptr = (volatile unsigned char *)block;
-	for (size_t i = 0; i < aligned; i++) {
-		ptr[i] = 0;
+    arena_block_t *block = malloc(total_size);
+    if (!block) return nullptr;
+
+    memset(block, 0, total_size);
+    __asm__ volatile("" : : : "memory");
+
+    block->id = 0;
+    block->is_wiped = false;
+    block->next = nullptr;
+    block->prev = nullptr;
+    block->offset = 0;
+    block->capacity = data_size;
+    return block;
+}
+
+static void internal_block_shred(arena_block_t *block, bool request_reset_offset) {
+	memset(block->data, 0, block->capacity);
+	__asm__ volatile("" : : : "memory");
+	block->is_wiped = true;
+
+	if (request_reset_offset) {
+		block->offset = 0;
 	}
+}
 
-	block->id = 0;
-	block->is_wiped = false;
-	block->next = nullptr;
-	block->prev = nullptr;
-	block->offset = 0;
-	block->capacity = size;
-	return block;
+static status_t internal_arena_shred(arena_t *arena, arena_id_t id, bool request_reset_offset) {
+    if (id == ARENA_ID_ENTIRE) {
+        for (arena_block_t *it = arena->head; it; it = it->next) {
+            internal_block_shred(it, request_reset_offset);
+        }
+    } else if (id == ARENA_ID_CURRENT) {
+        if (arena->current) internal_block_shred(arena->current, request_reset_offset);
+    } else {
+        arena_block_t *target = get_arena(arena->head, id);
+        if (target) internal_block_shred(target, request_reset_offset);
+        else return asstatus(CAT_ARENA, CND_ABORT, CODE_ALLOC);
+    }
+    return asstatus(CAT_ARENA, CND_SUCCESS, CODE_CLEAR);
 }
 
 /********************
@@ -49,15 +75,15 @@ arena_t *arena_init(size_t block_size) {
 	arena_t *arena = malloc(sizeof(arena_t));
 	if (!arena) return nullptr;
 
-	volatile unsigned char *ptr = (volatile unsigned char *)arena;
-	for (size_t i = 0; i < sizeof(arena_t); i++) {
-		ptr[i] = 0;
-	}
-
+	memset(arena, 0, sizeof(arena_t));
+	__asm__ volatile("" : : : "memory");
+	pthread_mutex_init(&arena->mutex, nullptr);
+	
 	size_t bs = (block_size < 1) ? SIZE_BLOCK_DEFAULT : block_size;
 	arena->block_size = align(bs);
 	arena->head = arena_new_block(arena->block_size);
 	if (!arena->head) {
+		// TODO: require shered
 		free(arena);
 		return nullptr;
 	}
@@ -71,6 +97,7 @@ arena_t *arena_init(size_t block_size) {
 
 static inline void *arena_allocate_raw(arena_t *arena, size_t size) {
 	size_t aligned_size = align(size);
+	pthread_mutex_lock(&arena->mutex);
 	while (arena->current->offset + aligned_size > arena->current->capacity) {
 		if (arena->current->next) {
 			arena->current = arena->current->next;
@@ -93,66 +120,72 @@ static inline void *arena_allocate_raw(arena_t *arena, size_t size) {
 	void *ptr = &arena->current->data[arena->current->offset];
 	arena->current->offset += aligned_size;
 	if (arena->is_reset) arena->is_reset = false;
+	pthread_mutex_unlock(&arena->mutex);
 	return ptr;
 }
-
-// void *arena_alloc_impl(arena_t *arena, size_t size, const char *file, int line, const char *func) {
-// 	void *ptr = arena_allocate_raw(arena, size);
-// 	if (!ptr) return nullptr;
-
-// 	arena->stats.last_caller = func;
-// 	arena->stats.last_alloc_size = size;
-// 	(void)file;
-// 	(void)line;
-
-// 	return ptr;
-// }
 
 void *arena_alloc(arena_t *arena, size_t size) {
 	return arena_allocate_raw(arena, size);
 }
 
-status_t arena_set_contains_fd(arena_t *arena, bool require_set) {
+status_t arena_set_contains_fd(arena_t *arena, bool is_contain) {
 	if (!arena) {
 		return asstatus(CAT_VALUE, CND_FATAL, CODE_PARAM);
 	}
 
-	arena->contains_fd = require_set;
+	arena->contains_fd = is_contain;
 
 	return asstatus(CAT_ARENA, CND_SUCCESS, CODE_SET);
 }
 
-status_t arena_set_contains_db_connection(arena_t *arena, bool require_set) {
+status_t arena_set_contains_db_connection(arena_t *arena, bool is_contain) {
 	if (!arena) {
 		return asstatus(CAT_ARENA, CND_FATAL, CODE_PARAM);
 	}
 
-	arena->contains_db_handle = require_set;
+	pthread_mutex_lock(&arena->mutex);
+	arena->contains_db_handle = is_contain;
+	pthread_mutex_unlock(&arena->mutex);
 
 	return asstatus(CAT_ARENA, CND_SUCCESS, CODE_SET);
 }
 
-status_t arena_set_contains_resources(arena_t *arena, bool require_set) {
+status_t arena_set_contains_resources(arena_t *arena, bool is_contain) {
 	if (!arena) {
 		return asstatus(CAT_ARENA, CND_FATAL, CODE_PARAM);
 	}
 
-	arena->contains_db_handle = require_set;
-	arena->contains_fd = require_set;
+	pthread_mutex_lock(&arena->mutex);
+	arena->contains_db_handle = is_contain;
+	arena->contains_fd = is_contain;
+	pthread_mutex_unlock(&arena->mutex);
 
 	return asstatus(CAT_ARENA, CND_SUCCESS, CODE_SET);
+}
+
+status_t arena_is_contains_resources(arena_t *arena) {
+	if (!arena) {
+		return asstatus(CAT_ARENA, CND_FATAL, CODE_PARAM);
+	}
+
+	pthread_mutex_lock(&arena->mutex);
+	if (arena->contains_db_handle || arena->contains_fd) {
+		pthread_mutex_unlock(&arena->mutex);
+		return asstatus(CAT_ARENA, CND_SUCCESS, CODE_CONTAINS);
+	}
+	pthread_mutex_unlock(&arena->mutex);
+	return asstatus(CAT_ARENA, CND_FAILURE, CODE_CONTAINS);
 }
 
 void arena_reset(arena_t *arena) {
-	if (!arena) return;
-	arena_block_t *it = arena->head;
-	while (it) {
-		it->offset = 0;
-		block_shred(it, ARENA_REQUEST_RESET_OFFSET);
-		it = it->next;
-	}
-	arena->current = arena->head;
-	arena->is_reset = true;
+    if (!arena) return;
+
+    pthread_mutex_lock(&arena->mutex);
+    internal_arena_shred(arena, ARENA_ID_ENTIRE, ARENA_REQUEST_RESET_OFFSET); // internal版を呼ぶ！
+
+    arena->current = arena->head;
+    arena->is_reset = true;
+    pthread_mutex_unlock(&arena->mutex);
 }
 
 size_t get_amount_capacity(arena_block_t *head) {
@@ -174,56 +207,84 @@ size_t get_amount_capacity(arena_block_t *head) {
 /**
 * If id < 0, reference current id
 */
-status_t arena_shred(arena_t *arena, unsigned short id, bool request_reset_offset) {
-	if (!arena) return asstatus(CAT_ARENA, CND_FATAL, CODE_PARAM);
-
-	unsigned short id_target = (id < 0) ? arena->current->id : id;
-	arena_block_t *block_target = get_arena(arena->head, id_target);
-
-	if (block_target == nullptr) return asstatus(CAT_ARENA, CND_ABORT, CODE_ALLOC);
-
-	volatile unsigned char *p = (volatile unsigned char *)block_target->data;
-	for (size_t i = 0; i < block_target->capacity; i++) {
-		p[i] = 0;
-	}
-	block_target->is_wiped = true;
-
-	if (request_reset_offset) {
-		block_target->offset = 0;
-	}
-
-	return asstatus(CAT_ARENA, CND_SUCCESS, CODE_CLEAR);
-}
-
-status_t block_shred(arena_block_t *block, bool request_reset_offset) {
-	if (!block) return asstatus(CAT_ARENA, CND_FATAL, CODE_PARAM);
-
-	volatile unsigned char *p = (volatile unsigned char *)block->data;
-	for (size_t i = 0; i < block->capacity; i++) {
-		p[i] = 0;
-	}
-	block->is_wiped = true;
-
-	if (request_reset_offset) {
-		block->offset = 0;
-	}
-
-	return asstatus(CAT_ARENA, CND_SUCCESS, CODE_CLEAR);
+status_t arena_shred(arena_t *arena, arena_id_t id, bool request_reset_offset) {
+    if (!arena) return asstatus(CAT_ARENA, CND_FATAL, CODE_PARAM);
+    pthread_mutex_lock(&arena->mutex);
+    status_t st = internal_arena_shred(arena, id, request_reset_offset);
+    pthread_mutex_unlock(&arena->mutex);
+    return st;
 }
 
 status_t arena_destroy(arena_t *arena, bool force_destory) {
-	if (!arena) return asstatus(CAT_ARENA, CND_FATAL, CODE_PARAM);
-	if (!force_destory && (arena->contains_db_handle || arena->contains_fd)) {
-		return asstatus(CAT_ARENA, CND_FAILURE, CODE_ARENA_FAILURE_RESOURCE_HELD);
-	}
+    if (!arena) return asstatus(CAT_ARENA, CND_FATAL, CODE_PARAM);
+    
+    pthread_mutex_lock(&arena->mutex);
+    
+    if (!force_destory && (arena->contains_db_handle || arena->contains_fd)) {
+        pthread_mutex_unlock(&arena->mutex);
+        return asstatus(CAT_ARENA, CND_FAILURE, CODE_ARENA_FAILURE_RESOURCE_HELD);
+    }
 
-	arena_block_t *block = arena->head;
+    arena_block_t *block = arena->head;
+    while (block) {
+        arena_block_t *next = block->next;
+        internal_block_shred(block, ARENA_REQUEST_RESET_OFFSET);
+        free(block);
+        block = next;
+    }
+    
+    pthread_mutex_unlock(&arena->mutex);
+    pthread_mutex_destroy(&arena->mutex);
+    free(arena);
+    return asstatus(CAT_ARENA, CND_SUCCESS, CODE_DESTROY);
+}
+
+char *arena_strdup(arena_t *arena, const char *src) {
+    if (!src) return NULL;
+    size_t len = strlen(src) + 1;
+    char *dest = (char *)arena_alloc(arena, len);
+    if (dest) {
+        memcpy(dest, src, len);
+    }
+    return dest;
+}
+
+/*
+	06/27/2026
+	Future implementation: arena_detach_block(arena_t *a, arena_id_t id);
+	
+	Data block `arena_block_t` in `arena_t` has data memory chain.
+	This function implemented if need to detach field to manage alone arena_block_t.
+
+	Or this,
+	typedef struct arena_block {
+		unsigned short id;
+		bool is_wiped;
+		bool is_persistent; /// ADD THIS
+		mem_provider_t provider;
+		struct arena_block *next;
+		struct arena_block *prev;
+		size_t offset;
+		size_t capacity;
+		alignas(16) unsigned char data[];
+	} arena_block_t;
+
+	If set is_persistent true,
+	auto detach and remain stand alone memory field and a function returns the ptr.
+
+	And like this finally in `destroy_arena()`:
+	```
 	while (block) {
 		arena_block_t *next = block->next;
-		block_shred(block, ARENA_REQUEST_RESET_OFFSET);
-		free(block);
+		if (!block->is_persistent) { // CHECK is_persistent
+			block_shred(arena, block->id, ARENA_REQUEST_RESET_OFFSET);
+			free(block);
+		} else {
+			// and detach here,
+			// then return ptr.
+		}
 		block = next;
 	}
-	free(arena);
-	return asstatus(CAT_ARENA, CND_SUCCESS, CODE_DESTROY);
-}
+	```
+*/
+
