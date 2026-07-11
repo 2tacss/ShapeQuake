6cbae393809f3738ad33f32e315f37a1db1ac4d4
2racss <vahamut@disroot.org>
2026-07-12 00:38:58 +0900

    memory/heap: fallback all of the fixes

--- include/memory/heap.h
+++ include/memory/heap.h
@@ -1,27 +1,33 @@
-#pragma once
-#include "heap.h"
+#ifndef HEAP_H_
+#define HEAP_H_
+
+#include <unistd.h>
 #include <pthread.h>
-#include <stddef.h>
-#include <stdint.h>
+#include "status.h"
 
-#define MAX_HEAPS 64
+/* Paging size */
+static inline size_t align_heap(size_t size) {
+	return (size + 15) & ~15;
+}
 
-typedef struct heap_slot_t heap_slot_t;
-typedef struct heap_tracker_t heap_tracker_t;
+constexpr size_t MAX_HEAPS = 20;
 
-struct heap_slot_t {
-	void *ptr;
-};
-
-struct heap_tracker_t {
-	heap_slot_t slots[MAX_HEAPS];
-	uint64_t used_mask;
-	size_t block_size;
-	size_t allocated;
+typedef struct {
 	pthread_mutex_t mutex;
-};
+	size_t active;
+	size_t allocated;
+	unsigned char *ptr[MAX_HEAPS];
+} heap_tracker_t;
 
-void tracker_init(heap_tracker_t *tracker, size_t block_size);
-int heap_alloc(heap_tracker_t *tracker);
-void heap_free(heap_tracker_t *tracker, int idx);
-void *heap_get_ptr(heap_tracker_t *tracker, int idx);
+/*******************
+ * Heap Management *
+ *******************/
+void tracker_init(heap_tracker_t *tracker);
+void tracker_destroy(heap_tracker_t *tracker);
+[[nodiscard]] void *heap_alloc(heap_tracker_t *tracker, size_t size);
+status_t heap_free(heap_tracker_t *tracker, void *ptr);
+void heap_free_all(heap_tracker_t *tracker);
+status_t tracking_health(heap_tracker_t *tracker);
+status_t rest_of_heap(heap_tracker_t *tracker);
+
+#endif
--- src/memory/heap.c
+++ src/memory/heap.c
@@ -1,94 +1,131 @@
 #include "memory/heap.h"
+#include "status.h"
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
-void tracker_init(heap_tracker_t *tracker, size_t block_size) {
-	memset(tracker, 0, sizeof(heap_tracker_t));
+static void *internal_heap_alloc(heap_tracker_t *tracker, size_t required_size) {
+	if (!tracker || required_size < 1) return nullptr;
+	if (tracker->active >= MAX_HEAPS) return nullptr;
+
+	size_t req_aligned = align_heap(required_size);
+	size_t total_aligned_size = req_aligned + 16;
+	void *ptr = malloc(total_aligned_size);
+	if (!ptr) return nullptr;
+
+	memset(ptr, 0, total_aligned_size);
 	__asm__ volatile("" : : : "memory");
-	tracker->block_size = block_size;
+	*(size_t *)ptr = req_aligned;
+
+	void *user_ptr = (unsigned char *)ptr + 16;
+	tracker->ptr[tracker->active] = user_ptr;
+	tracker->active++;
+	tracker->allocated += req_aligned;
+
+	return user_ptr;
+}
+
+/*******************
+ * Heap Management *
+ *******************/
+[[nodiscard]]
+status_t tracking_health(heap_tracker_t *tracker) {
+	if (!tracker) {
+		return asstatus(CAT_HEAP_TRACKER, CND_ABORT, CODE_PARAM);
+	}
+	
+	if ((tracker->active == 0 && tracker->allocated) || (tracker->active > 0 && !tracker->allocated)) {
+		return asstatus(CAT_HEAP_TRACKER, CND_FATAL, CODE_CALCULATION);
+	} else {
+		return asstatus(CAT_HEAP_TRACKER, CND_SUCCESS, CODE_CALCULATION);
+	}
+}
+
+void tracker_init(heap_tracker_t *tracker) {
+	if (!tracker) return;
+	for (size_t i = 0; i < MAX_HEAPS; i++) {
+		tracker->ptr[i] = nullptr;
+	}
 	pthread_mutex_init(&tracker->mutex, nullptr);
 }
 
-int heap_alloc(heap_tracker_t *tracker) {
-	if (!tracker) return -1;
-
+void *heap_alloc(heap_tracker_t *tracker, size_t size) {
+	if (!tracker) return nullptr;
 	pthread_mutex_lock(&tracker->mutex);
-
-	if (tracker->used_mask == 0xFFFFFFFFFFFFFFFFULL) {
-		pthread_mutex_unlock(&tracker->mutex);
-		return -1;
-	}
-
-	uint64_t free_mask = ~tracker->used_mask;
-	int found_idx = __builtin_ctzll(free_mask);
-
-	if (found_idx < 0 || found_idx >= MAX_HEAPS) {
-		pthread_mutex_unlock(&tracker->mutex);
-		return -1;
-	}
-
-	void *ptr = malloc(tracker->block_size);
-	if (!ptr) {
-		pthread_mutex_unlock(&tracker->mutex);
-		return -1;
-	}
-
-	memset(ptr, 0, tracker->block_size);
-	__asm__ volatile("" : : : "memory");
-	
-	tracker->slots[found_idx].ptr = ptr;
-	tracker->used_mask |= (1ULL << found_idx);
-	tracker->allocated += tracker->block_size;
-
+	void *ptr = internal_heap_alloc(tracker, size);
 	pthread_mutex_unlock(&tracker->mutex);
-	return found_idx;
-}
-
-void heap_free(heap_tracker_t *tracker, int idx) {
-	if (!tracker || idx < 0 || idx >= MAX_HEAPS) return;
-
-	pthread_mutex_lock(&tracker->mutex);
-
-	if (tracker->used_mask & (1ULL << idx)) {
-		if (tracker->slots[idx].ptr) {
-			memset(tracker->slots[idx].ptr, 0, tracker->block_size);
-			free(tracker->slots[idx].ptr);
-			tracker->slots[idx].ptr = nullptr;
-		}
-		tracker->used_mask &= ~(1ULL << idx);
-		tracker->allocated -= tracker->block_size;
-	}
-
-	pthread_mutex_unlock(&tracker->mutex);
+	return ptr;
 }
 
 void heap_free_all(heap_tracker_t *tracker) {
 	if (!tracker) return;
-
+	
 	pthread_mutex_lock(&tracker->mutex);
-
-	uint64_t mask = tracker->used_mask;
-
-	while (mask > 0) {
-		int i = __builtin_ctzll(mask);
-
-		if (tracker->slots[i].ptr) {
-			memset(tracker->slots[i].ptr, 0, tracker->block_size);
+	for (size_t i = 0; i < tracker->active; i++) {
+		if (tracker->ptr[i]) {
+			void *raw_ptr = (unsigned char *)tracker->ptr[i] - 16;
+			size_t size = *(size_t *)raw_ptr;
+			
+			memset(raw_ptr, 0, size + 16);
 			__asm__ volatile("" : : : "memory");
-			free(tracker->slots[i].ptr);
-			tracker->slots[i].ptr = nullptr;
+			
+			free(raw_ptr);
+			tracker->ptr[i] = nullptr;
 		}
-
-		mask &= (mask - 1);
 	}
-
-	tracker->used_mask = 0;
+	tracker->active = 0;
 	tracker->allocated = 0;
-
 	pthread_mutex_unlock(&tracker->mutex);
 }
 
-void *heap_get_ptr(heap_tracker_t *tracker, int idx) {
-	if (!tracker || idx < 0 || idx >= MAX_HEAPS) return nullptr;
-	return (tracker->used_mask & (1ULL << idx)) ? tracker->slots[idx].ptr : nullptr;
+status_t heap_free(heap_tracker_t *tracker, void *ptr) {
+	if (!ptr) return asstatus(CAT_HEAP, CND_SUCCESS, CODE_FREE);
+	if (!tracker) return asstatus(CAT_HEAP, CND_ABORT, CODE_PARAM);
+
+	pthread_mutex_lock(&tracker->mutex);
+	void *raw_ptr = (unsigned char *)ptr - 16;
+	size_t req_aligned = *(size_t *)raw_ptr;
+	size_t total_aligned_size = req_aligned + 16;
+
+	if (tracker->active == 0 || tracker->allocated < req_aligned) {
+		pthread_mutex_unlock(&tracker->mutex);
+		return asstatus(CAT_HEAP, CND_ABORT, CODE_CALCULATION);
+	}
+
+	bool found = false;
+	for (size_t i = 0; i < MAX_HEAPS; i++) {
+		if (tracker->ptr[i] == ptr) {
+			tracker->ptr[i] = nullptr;
+			found = true;
+			break;
+		}
+	}
+
+	if (!found) {
+		pthread_mutex_unlock(&tracker->mutex);
+		return asstatus(CAT_HEAP, CND_ABORT, CODE_NOT_FOUND);
+	}
+
+	memset(raw_ptr, 0, total_aligned_size);
+	__asm__ volatile("" : : : "memory");
+
+	free(raw_ptr);
+	
+	tracker->active--;
+	tracker->allocated -= req_aligned;
+
+	pthread_mutex_unlock(&tracker->mutex);
+	return asstatus(CAT_HEAP, CND_SUCCESS, CODE_FREE);
+}
+
+status_t rest_of_heap(heap_tracker_t *tracker) {
+	if (!tracker) {
+		return asstatus(CAT_HEAP_TRACKER, CND_ABORT, CODE_PARAM);
+	}
+
+	if (tracker->active > 0 && tracker->allocated) {
+		return asstatus(CAT_HEAP, CND_INFO, CODE_EXIST);
+	} else {
+		return asstatus(CAT_HEAP, CND_INFO, CODE_NO_EXIST);
+	}
 }
